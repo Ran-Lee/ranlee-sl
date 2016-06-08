6efc915afa038cd73c1da1a8ed00ee60e1a3fcbd
email <1244748504@qq.com>
2016-06-08 22:46:52 +0800

    LinkedList 练习

---
+++ Main3.java
@@ -0,0 +1,45 @@
+package com.orilore.lr.test1;
+
+import java.util.LinkedList;
+
+import java.util.Iterator;
+
+public class Main3 {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     
+		LinkedList<Student> l1=new LinkedList<Student>();
+		
+		Student a1=new Student("1313","wang");
+		
+		Student a2=new Student("1314","lee");		
+		
+		Student a3=new Student("1315","zhang");
+		
+		Student a4=new Student("1316","liu");
+		
+		l1.add(a1);
+		
+		l1.add(a2);
+
+		l1.addLast(a3);
+		
+		l1.addFirst(a4);    //相比较ArrayList多了 addFirst  addLast
+		
+		l1.remove(0);
+		
+		l1.remove(0);
+
+		l1.removeLast();
+		
+		for(Object x:l1){
+			
+			System.out.println(x);
+			
+		}
+		
+	}
+
+}
---
+++ Student.java
@@ -0,0 +1,19 @@
+package com.orilore.lr.test1;
+
+public class Student {
+	
+	private String name;
+	
+	private String number;
+	
+	public Student(String number,String name){
+		
+		this.name=name;
+		
+		this.number=number;
+	}
+	
+	
+	
+
+}

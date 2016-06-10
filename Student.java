9153199d51a237d9a443eb19adcc940d11df9c84
email <1244748504@qq.com>
2016-06-10 23:01:24 +0800

    Student.java修改

--- Student.java
+++ Student.java
@@ -1,29 +1,61 @@
 package com.orilore.lr.test1;
 
 public class Student {
-	
+
 	private String name;
-	
+
 	private String number;
-	
-	public Student(String number,String name){
-		
-		this.name=name;
-		
-		this.number=number;
+
+	public Student(String number, String name) {
+
+		this.name = name;
+
+		this.number = number;
 	}
-	public String getName(){
-		
-		return  name;
-		
+
+	public String getName() {
+
+		return name;
+
 	}
-	
-	public String setName(String name){
-		
-		return this.name=name;
-		
+
+	public void setName(String name) {
+
+		this.name = name;
+
 	}
-	
-	
+
+	public String getNumber() {
+
+		return number;
+
+	}
+
+	public void setNumber() {
+
+		this.number = number;
+
+	}
+
+	public int hashCode() {      //规定了固定的hashCode
+
+		return 1;
+
+	}
+
+	public boolean equals(Object o) {
+
+		boolean f = false;
+
+		Student stu = (Student) o;
+
+		if (this.number.equals(stu.getNumber()) && this.name.equals(stu.getName())) {
+
+			f = true;
+		}
+
+		return f;
+
+	}
 
 }
